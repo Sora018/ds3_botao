1950471e06c557cb9862beded86534581e886d31
Renan M.C <140671219+Sora018@users.noreply.github.com>
2024-03-25 15:07:41 -0300

    projeto

---
+++ javas.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const drawerToggleBtn = document.getElementById('drawerToggleBtn');
+    const drawer = document.getElementById('drawer');
+  
+    drawerToggleBtn.addEventListener('click', function() {
+      drawer.classList.toggle('open');
+      drawerToggleBtn.classList.toggle('hide');
+    });
+  
+    document.addEventListener('click', function(event) {
+      const isClickInsideDrawer = drawer.contains(event.target);
+      const isClickInsideToggleButton = drawerToggleBtn.contains(event.target);
+  
+      if (!isClickInsideDrawer && !isClickInsideToggleButton && drawer.classList.contains('open')) {
+        drawer.classList.remove('open');
+        drawerToggleBtn.classList.remove('hide');
+      }
+    });
+
+    const img = document.createElement("img");
+    img.src = "imagens/i747410.jpeg";
+    img.style.position = "fixed";
+    img.style.top = "0";
+    img.style.left = "0";
+    img.style.width = "100vw";
+    img.style.height = "100vh";
+    
+    document.body.appendChild(img);
+    
+});
